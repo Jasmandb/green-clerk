424e55112d032fce58d4f4ba4ef46256c0541e64
Osama-Elsamny <osama_el_samny@sfu.ca>
2020-03-20 11:31:56 -0700

    Added changes to fix bugs seen during the integration

--- decision-making/src/tasks/item_detection.py
+++ decision-making/src/tasks/item_detection.py
@@ -12,7 +12,7 @@ class ItemDetection:
     def __init__(self):
         self.connection = None
         self.ard_api = None
-        self.ultrasonic = None
+        self.ultrasonics = []
         self.distance = defaultdict()
         self.first_meas = True
         self.item_detected = False
@@ -27,43 +27,43 @@ class ItemDetection:
             self.connection = SerialManager(device='/dev/ttyACM1')
             self.ard_api = ArduinoApi(connection=self.connection)
         except Exception as e:
-            logging.error('Failed to connect to Arduino {}'.format(str(e)))
+            logger.error('Failed to connect to Arduino {}'.format(str(e)))
             raise e
 
     def setup_pin_modes(self):
         for ping_pin, echo_pin in Pins.ULTRASONIC_PINS:
-            self.ard_api.pinMode(ping_pin, self.ard_api.OUPUT)
+            self.ard_api.pinMode(ping_pin, self.ard_api.OUTPUT)
             self.ard_api.pinMode(echo_pin, self.ard_api.INPUT)
 
         for ir_pin in Pins.IR_PINS:
             self.ard_api.pinMode(ir_pin, self.ard_api.INPUT)
             self.ard_api.digitalWrite(ir_pin, self.ard_api.HIGH)
 
+    def setup_ultrasonic_obj(self):
+        for ping_pin, echo_pin in Pins.ULTRASONIC_PINS:
+            self.ultrasonics.append((Ultrasonic(echo_pin, ping_pin, False, connection=self.connection), ping_pin))
+
     def run(self):
         logger.debug('Reading Information from the Ultrasonic sensors')
+        self.setup_ultrasonic_obj()
         while not self.item_detected:
-            for ping_pin, echo_pin in Pins.ULTRASONIC_PINS:
-                self.ultrasonic = Ultrasonic(echo_pin, ping_pin, False, connection=self.connection)
-                self.distance[ping_pin] = self.ultrasonic.get_distance()
+            for ultrasonic, ping_pin in self.ultrasonics:
+                self.distance[ping_pin] = ultrasonic.get_distance()
                 time.sleep(0.001)
 
             if self.first_meas:
                 self.first_meas = False
                 self.base_meas = deepcopy(self.distance)
 
-            for ping_pin, echo_pin in Pins.ULTRASONIC_PINS:
-                if abs(self.base_meas[ping_pin] - self.first_meas[ping_pin]) > 2:
+            for ultrasonic, ping_pin, in self.ultrasonics:
+                if abs(self.base_meas[ping_pin] - self.distance[ping_pin]) > 2:
                     self.item_detected = True
                     logger.debug(
-                        'Ultrasonic detection with ping_pin, echo_pin, reading: {}, {}, {}'.format(ping_pin, echo_pin,
-                                                                                                   self.base_meas[
-                                                                                                       ping_pin]))
+                        'Ultrasonic detection with ping_pin, reading: {}, {}'.format(ping_pin, self.distance[ping_pin]))
             for ir_pin in Pins.IR_PINS:
                 if self.ard_api.digitalRead(ir_pin) == self.ard_api.LOW:
                     self.item_detected = True
                     logger.debug('IR sensor detected an item')
-            # TODO: Confirm with @Amneet: Why do we need to sleep for a second between each iteration?
-            time.sleep(1)
 
 
 if __name__ == '__main__':
