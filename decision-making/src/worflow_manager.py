93f58654e18ad5c5cb0fcb44b896757a8ad57de1
Osama-Elsamny <osama_el_samny@sfu.ca>
2020-04-04 18:42:16 -0700

    removed infinte loop for now

--- decision-making/src/worflow_manager.py
+++ decision-making/src/worflow_manager.py
@@ -4,7 +4,6 @@ from src.steps.classify import Classify
 from src.steps.drop import Drop
 from src.tasks.item_detection import ItemDetection
 
-
 logger = logging.getLogger(__name__)
 
 
@@ -14,17 +13,16 @@ class WorkflowManager:
         self.waste = None
 
     def start(self):
-        while True:
-            logger.debug('Running Item Detection')
-            self.item_detection.run()
-            logger.debug('A new item has been inserted')
-            self.waste = Waste()
-            classify_step = Classify(self.waste)
-            classify_step.run()
-            logger.debug('Classify step is done and the determined the waste type to be {}'.format(self.waste.type))
-            drop_step = Drop(self.waste)
-            self.waste.status = drop_step.status
-            logger.debug('Dropping step is done with status {} and waiting for a new item'.format(drop_step.status))
+        logger.debug('Running Item Detection')
+        self.item_detection.run()
+        logger.debug('A new item has been inserted')
+        self.waste = Waste()
+        classify_step = Classify(self.waste)
+        classify_step.run()
+        logger.debug('Classify step is done and the determined the waste type to be {}'.format(self.waste.type))
+        drop_step = Drop(self.waste)
+        self.waste.status = drop_step.status
+        logger.debug('Dropping step is done with status {} and waiting for a new item'.format(drop_step.status))
 
 
 if __name__ == '__main__':
