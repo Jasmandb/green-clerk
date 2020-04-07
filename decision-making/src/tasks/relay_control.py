3022bc3314e36db104c380d4e92808fa1eecaaaa
Osama-Elsamny <osama_el_samny@sfu.ca>
2020-04-06 19:22:37 -0700

    Using a relay API to communicate with ard

--- decision-making/src/tasks/relay_control.py
+++ decision-making/src/tasks/relay_control.py
@@ -1,5 +1,5 @@
 from src.app_config import RelayStates, Pins, Arduino, logging
-from nanpy import ArduinoApi, SerialManager
+from nanpy import ArduinoApi, SerialManager, Relay
 
 logger = logging.getLogger(__name__)
 
@@ -7,18 +7,21 @@ logger = logging.getLogger(__name__)
 class RelayControl:
     def __init__(self):
         self.ard_api = None
+        self.relay = None
         self.ard_id = Arduino.ard_3
         self.connection = None
         self.create_connection_channel()
-        self.setup_pin_modes()
+        self.setup_relay_obj()
 
     def run(self, state):
         if state == RelayStates.OPEN:
             logger.debug('Opening the lid requested state is {}'.format(state))
-            self.ard_api.digitalWrite(Pins.RELAY_PINS[0], self.ard_api.LOW)
+            # self.ard_api.digitalWrite(Pins.RELAY_PINS[0], self.ard_api.LOW)
+            self.relay.open()
         else:
             logger.debug('Closing the lid requested state is state {}'.format(state))
             self.ard_api.digitalWrite(Pins.RELAY_PINS[0], self.ard_api.HIGH)
+            self.relay.close()
 
     def create_connection_channel(self):
         try:
@@ -29,10 +32,11 @@ class RelayControl:
             logging.error('Failed to connect to ard_id: {} and error: {}'.format(self.ard_id, str(e)))
             raise e
 
-    def setup_pin_modes(self):
+    def setup_relay_obj(self):
         # TODO: Since we are not expected to expand our number of relays this class is only coded for one sensor
         # TODO: extend to have the ability to expand (I don't think this is necessary though)
-        self.ard_api.pinMode(Pins.RELAY_PINS[0], self.ard_api.OUTPUT)
+        # self.ard_api.pinMode(Pins.RELAY_PINS[0], self.ard_api.OUTPUT)
+        self.relay = Relay(Pins.RELAY_PINS[0], self.connection)
 
     def close_ard_connection(self):
         self.connection.close()
