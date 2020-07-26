20bd7b137af2158f57045e953e87021f17a27fb2
Osama-Elsamny <osama_el_samny@sfu.ca>
2020-07-25 21:03:05 -0700

    update relay to have pin as an input

--- decision-making/src/tasks/relay_control.py
+++ decision-making/src/tasks/relay_control.py
@@ -5,8 +5,9 @@ logger = logging.getLogger(__name__)
 
 
 class RelayControl:
-    def __init__(self):
+    def __init__(self, pin=None):
         self.ard_api = None
+        self.pin = None
         self.relay = None
         self.ard_id = Arduino.ard_3
         self.connection = None
@@ -16,11 +17,9 @@ class RelayControl:
         self.setup_relay_obj()
         if state == RelayStates.OPEN:
             logger.debug('Opening the lid requested state is {}'.format(state))
-            # self.ard_api.digitalWrite(Pins.RELAY_PINS[0], self.ard_api.LOW)
             self.relay.open()
         else:
             logger.debug('Closing the lid requested state is state {}'.format(state))
-            # self.ard_api.digitalWrite(Pins.RELAY_PINS[0], self.ard_api.HIGH)
             self.relay.close()
         self.close_ard_connection()
 
@@ -34,10 +33,7 @@ class RelayControl:
             raise e
 
     def setup_relay_obj(self):
-        # TODO: Since we are not expected to expand our number of relays this class is only coded for one sensor
-        # TODO: extend to have the ability to expand (I don't think this is necessary though)
-        # self.ard_api.pinMode(Pins.RELAY_PINS[0], self.ard_api.OUTPUT)
-        self.relay = Relay(Pins.RELAY_PINS[0], self.connection)
+        self.relay = Relay(self.pin, self.connection)
 
     def close_ard_connection(self):
         self.connection.close()
