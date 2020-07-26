91b16d13de9b44bedf862256435a2a21cd2c89ac
Osama-Elsamny <osama_el_samny@sfu.ca>
2020-07-25 20:30:19 -0700

    completed door control nanpy FW

--- decision-making/src/app_config.py
+++ decision-making/src/app_config.py
@@ -22,6 +22,7 @@ class Pins(list, Enum):
     RELAY_PINS = [7]
     SERVO_PINS = [9]
     WEIGHT_PINS = [(14, 15, -1096.65)]
+    DOOR = [9, 13, 8]
 
 
 Arduino = {'item_detection': None, 'classification_sensors': None}
--- decision-making/src/tasks/door_control.py
+++ decision-making/src/tasks/door_control.py
@@ -1,5 +1,5 @@
 from src.app_config import States, Pins, Arduino, logging
-from nanpy import ArduinoApi, SerialManager, Relay
+from nanpy import ArduinoApi, SerialManager, DoorCont
 
 logger = logging.getLogger(__name__)
 
@@ -7,21 +7,19 @@ logger = logging.getLogger(__name__)
 class DoorControl:
     def __init__(self):
         self.ard_api = None
-        self.relay = None
+        self.door_control = None
         self.ard_id = Arduino.ard_3
         self.connection = None
 
     def run(self, state):
         self.create_connection_channel()
-        self.setup_relay_obj()
+        self.setup_door_control_obj()
         if state == States.OPEN:
             logger.debug('Opening the door requested state is {}'.format(state))
-            # self.ard_api.digitalWrite(Pins.RELAY_PINS[0], self.ard_api.LOW)
-            self.relay.open()
+            self.door_control.open_door()
         else:
             logger.debug('Closing the door requested state is state {}'.format(state))
-            # self.ard_api.digitalWrite(Pins.RELAY_PINS[0], self.ard_api.HIGH)
-            self.relay.close()
+            self.door_control.close_door()
         self.close_ard_connection()
 
     def create_connection_channel(self):
@@ -33,8 +31,8 @@ class DoorControl:
             logging.error('Failed to connect to ard_id: {} and error: {}'.format(self.ard_id, str(e)))
             raise e
 
-    def setup_relay_obj(self):
-        pass
+    def setup_door_control_obj(self):
+        self.door_control = DoorCont(Pins.DOOR[0], Pins.DOOR[1], Pins.DOOR[2], self.connection)
 
     def close_ard_connection(self):
         self.connection.close()
