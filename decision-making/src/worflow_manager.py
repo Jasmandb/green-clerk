d4996f9452669ce2e0085512c233616e1ba05f23
Osama-Elsamny <osama_el_samny@sfu.ca>
2020-08-12 12:59:30 -0700

    updated the workflow manager to accomdate corner cases

--- decision-making/src/worflow_manager.py
+++ decision-making/src/worflow_manager.py
@@ -2,6 +2,7 @@ from src.app_config import Waste, logging, Arduino, States, Pins, ConnectionMana
 from src.steps.classify import Classify
 from src.steps.drop import Drop
 from src.tasks.arduino_manager import ArduinoManager
+from src.tasks.bin_level import BinLevel
 from src.tasks.communication_manager import CommunicationManager
 from src.tasks.door_control import DoorControl
 from src.tasks.item_detection import ItemDetection
@@ -18,19 +19,37 @@ class WorkflowManager:
     # 2) classify the item
     # 3) Drop the item
     def start(self):
+        # part of the boot up, figuring out which arduino is which
         arduino_manager = ArduinoManager()
         arduino_manager.run()
-        self.start_all_ard_connections()
-        self.confirm_door_closed()
-
+        just_started = True
         while True:
+            logger.debug('Stating all Arduinos connection')
+            self.start_all_ard_connections()
+            logger.debug('Confirming that the door is closed and magnets are engaged')
+            self.confirm_door_closed()
+
+            # 1) Turn the System lights to green
+            self.change_system_state(States.OPEN)
+
+            if just_started:
+                just_started = False
+                bin_level = BinLevel(ConnectionManager['mechanical'])
+                bin_level.run()
+                if bin_level.bin_full:
+                    self.change_system_state(States.CLOSE)
+                    self.wait_until_bins_empty()
+                    logger.debug('The bin are full, the system is pausing')
+
             logger.debug('Running Item Detection')
-            self.change_system_status(States.OPEN)
+            # 2) start the item detection script
             item_detection = ItemDetection(ConnectionManager['detect_item'].ard_api,
                                            ConnectionManager['detect_item'].connection)
             item_detection.run()
             logger.debug('A new item has been inserted')
-            self.change_system_status(States.CLOSE)
+            # 3) If an new item is detected change the system lights to red
+            self.change_system_state(States.CLOSE)
+            # 4) then run thw classify and drop class
             waste = Waste()
             classify_step = Classify(waste)
             classify_step.run()
@@ -38,11 +57,15 @@ class WorkflowManager:
             drop_step = Drop(waste)
             drop_step.run()
             waste.status = drop_step.status
+            logger.debug('Dropping step is done with status {} and waiting for a new item'.format(drop_step.status))
+            # 5) if bins are full we need to hold the system until it is rebooted (basically infinite loop)
             if drop_step.system_hold:
                 self.wait_until_bins_empty()
-            logger.debug('Dropping step is done with status {} and waiting for a new item'.format(drop_step.status))
+                logger.debug('The bin are full, the system is pausing')
+            logger.debug('closing all the Arduinos connection')
+            self.close_all_ard_connections()
 
-    def change_system_status(self, state):
+    def change_system_state(self, state):
         light_control = LightControl(ConnectionManager['mechanical'].connection, Pins.SYSTEM_LIGHT[0])
         light_control.run(state)
 
@@ -61,6 +84,11 @@ class WorkflowManager:
         ConnectionManager['mechanical'] = com_manager
         com_manager = CommunicationManager(Arduino['classification'])
         ConnectionManager['classification'] = com_manager
+
+    def close_all_ard_connections(self):
+        ConnectionManager['detect_item'].close_ard_connection()
+        ConnectionManager['mechanical'].close_ard_connection()
+        ConnectionManager['classification'].close_ard_connection()
 
 
 if __name__ == '__main__':
