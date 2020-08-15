0dfc330242d2aeb8767e404d678d1dcfbf66368c
Osama-Elsamny <osama_el_samny@sfu.ca>
2020-08-15 16:16:38 -0700

    updated boot up

--- decision-making/boot_up/boot_up.py
+++ decision-making/boot_up/boot_up.py
@@ -1,4 +1,4 @@
 import os
 
-os.chdir('cd /home/pi/product-name-tbd/decision-making')
-os.system('source venv/bin/activate & sudo python src/workflow_manager.py &')
+os.chdir('/home/pi/product-name-tbd/decision-making')
+os.system('ls -al\nsource venv/bin/activate\nsudo python src/workflow_manager.py')
