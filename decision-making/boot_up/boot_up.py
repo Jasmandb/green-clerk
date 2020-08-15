565841f9da1d707b7ceb0197e47f43ba9fdf5c14
Osama-Elsamny <osama_el_samny@sfu.ca>
2020-08-15 15:41:45 -0700

    Updated user to pi

--- decision-making/boot_up/boot_up.py
+++ decision-making/boot_up/boot_up.py
@@ -1,5 +1,5 @@
 import os
 
-os.chdir("/home/user/product-name-tbd/decision-making") #not sure what our user is or if we log in as root
+os.chdir("/home/pi/product-name-tbd/decision-making")
 os.system("source venv/bin/activate")
 os.system("sudo python src/workflow_manager.py")
