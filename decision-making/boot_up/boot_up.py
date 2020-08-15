849fa6c2a79912797d2ece9551dc9d51adf1db22
Osama-Elsamny <osama_el_samny@sfu.ca>
2020-08-15 16:00:56 -0700

    updated workflow manager to the right name

--- decision-making/boot_up/boot_up.py
+++ decision-making/boot_up/boot_up.py
@@ -2,4 +2,4 @@ import os
 
 os.chdir("/home/pi/product-name-tbd/decision-making")
 os.system("source venv/bin/activate")
-os.system("sudo python src/workflow_manager.py")
+os.system("sudo python src/workflow_manager.py &")
